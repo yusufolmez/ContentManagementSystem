c80c1b55cd99ba1c46f99cd0e71999ebf04ebc67
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-04-10 10:43:05 +0300

    feat: add CORS support for local development
    - Added default CORS policy for `http://localhost:3000` and `https://localhost:3000`
    - Allowed any headers and methods for development purposes
    - Registered the policy in `Program.cs` and added `app.UseCors()` to the middleware pipeline

--- Presentation/ContentManagementSystem.API/Program.cs
+++ Presentation/ContentManagementSystem.API/Program.cs
@@ -5,6 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddPersistanceSerives();
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+    policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+          .AllowAnyHeader()
+          .AllowAnyMethod()
+));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
